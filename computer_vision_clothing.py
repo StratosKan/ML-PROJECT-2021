05805748850549e480dad574bf0663f51f3c6872
Stratos Kanavitsas <41113259+KingNemesis@users.noreply.github.com>
2021-11-12 19:18:51 +0200

    minor edits

--- computer_vision_clothing.py
+++ computer_vision_clothing.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
+
+print(tf.__version__)
 data = tf.keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
 
-training_images = training_images / 255.0 #normalization
+training_images = training_images / 255.0  # normalization
 test_images = test_images / 255.0
 
 model = tf.keras.models.Sequential([
@@ -16,4 +18,8 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 model.fit(training_images, training_labels, epochs=5)
+
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
 
