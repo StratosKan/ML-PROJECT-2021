9ffdf906c60ccf9998c4a8b35ae1f96facec86aa
Stratos Kanavitsas <41113259+KingNemesis@users.noreply.github.com>
2021-11-16 03:42:04 +0200

    transfer learning inception v3

--- transfer_learning_inception_v3_google.py
+++ transfer_learning_inception_v3_google.py
@@ -1,2 +1,41 @@
+import urllib.request
+
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.optimizers import RMSprop
+
+weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
+
+weights_file = "inception_v3.h5"
+urllib.request.urlretrieve(weights_url, weights_file)
+
+pre_trained_model = InceptionV3(input_shape=(150, 150, 3),        # the architecture
+                                include_top=False,
+                                weights=None)
+
+pre_trained_model.load_weights(weights_file)  # loading weights
+
+# pre_trained_model.summary()                   # checking out the CNN
+# Total params: 21,802,784
+# Trainable params: 21,768,352
+# Non-trainable params: 34,432
+
+for layer in pre_trained_model.layers:                             # cropping the network (mixed7 == 7x7 images)
+    layer.trainable = False
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+model.compile(optimizer=RMSprop(learning_rate=0.0001),
+              loss='binary_crossentropy',
+              metrics=['acc'])
