e0f44e2eb53f5113e16be088294c40cb682c917f
Stratos Kanavitsas <41113259+KingNemesis@users.noreply.github.com>
2021-11-28 17:31:20 +0200

    Tensorflow Text Generator - Part 2
    Train Ready

--- irish_songs_text_generator.py
+++ irish_songs_text_generator.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
@@ -5,4 +6,62 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 import numpy as np
+import matplotlib.pyplot as plt
 
+tokenizer = Tokenizer()
+
+project_path = os.path.dirname(os.path.abspath(__file__))
+data_loc = '/tmp/irish_lyrics.txt'
+data_path = project_path + data_loc
+
+data = open(data_path).read()
+
+corpus = data.lower().split("\n")
+
+tokenizer.fit_on_texts(corpus)
+
+total_words = len(tokenizer.word_index) + 1
+
+# print(tokenizer.word_index)
+print("Total words: " + str(total_words))
+
+input_sequences = []
+for line in corpus:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# padding sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(
+    input_sequences, maxlen=max_sequence_len, padding='pre')
+)
+
+# create predictors and labels
+xs, labels = input_sequences[:, :-1], input_sequences[:, -1]
+
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+# MODEL CREATION
+embedding_dim = 8
+
+model = Sequential()
+model.add(Embedding(total_words, embedding_dim))
+model.add(Bidirectional(LSTM(max_sequence_len-1, return_sequences='True')))
+model.add(Bidirectional(LSTM(max_sequence_len-1)))
+model.add(Dense(total_words, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+history = model.fit(xs, ys, epochs=10, verbose=1)
+
+
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.show()
+
+
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
